7daab6ace4f568932ac039f277ff26832a6bbabf
Ashwani <ashwani.pandey@myqsoft.com>
2026-05-03 12:31:02 +0530

    fix: use provideZonelessChangeDetection for zoneless support

--- projects/crm/src/app/app.config.ts
+++ projects/crm/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +7,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
